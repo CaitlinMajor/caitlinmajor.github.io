998622ed991a4792d889a8e915a05a2d96273337
Caitlin Major <caitlin.m@skyship.tv>
2019-12-15 14:01:30 -0500

    dfg

--- libs/GlobalScripts.js
+++ libs/GlobalScripts.js
@@ -284,8 +284,8 @@ function addNextButton(){
 	nextButton = new lib.nextbutton_mc();
 
 	stage.addChild(nextButton);
-	width = window.screen.width + window.screen.width;
-	height = window.screen.height;
+	width = window.screen.width * 2;
+	height = window.screen.height *0.25;
 	nextButton.x = width;
 	nextButton.y = height
 	nextButton.alpha = 0;
@@ -326,7 +326,7 @@ function addPreviousButton(){
 
 	stage.addChild(previousButton);
 	width = window.screen.width - window.screen.width;
-	height = window.screen.height;
+	height = window.screen.height *0.25;
 	previousButton.x = width;
 	previousButton.y = height
 	previousButton.alpha = 0;
