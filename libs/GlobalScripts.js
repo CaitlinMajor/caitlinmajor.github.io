eba0ec6c20629b551df14c0caf43f86adf4c018b
Caitlin Major <caitlin.m@skyship.tv>
2020-03-12 10:51:44 -0400

    Testing swiping

--- libs/GlobalScripts.js
+++ libs/GlobalScripts.js
@@ -1,176 +1,82 @@
 var soundsLoaded = false;
 var firstTime = true;
 var nextButtonAdded = false;
-
-var sounds = {
-	preloadFiles: function(){
-		var audioFile;
-		var l = [];
-		for (var i = sounds.library.length - 1; i >= 0; i--) {
-			audioFile = sounds.library[i];
-			audioFile.instance = createjs.Sound.createInstance(audioFile.id);
-			l.push(audioFile);
-		};
-		sounds.library = l;
-	},
-	getInstance: function(id){
-		var audioFile;
-		for (var i = sounds.library.length - 1; i >= 0; i--) {
-			audioFile = sounds.library[i];
-			if(id == audioFile.id){
-				return audioFile.instance;
-			}
-		};
-		console.log("Couldn't find audio file with id: "+id);
-		return false;
-	},
-	library:[
-		{id:"grumbleAffirmative06", src:"sounds/grumble_affirmative_06.mp3"},
-		{id:"grumbleChattering04", src:"sounds/grumble_chattering_04.mp3"},
-		{id:"grumbleEating02", src:"sounds/grumble_eating_02.mp3"},
-		{id:"grumbleHappy03", src:"sounds/grumble_happy_03.mp3"},
-		{id:"grumbleScared07", src:"sounds/grumble_scared_07.mp3"},
-		{id:"humbleAffirmative03", src:"sounds/humble_affirmative_03.mp3"},
-		{id:"humbleEating01", src:"sounds/humble_eating_01.mp3"},
-		{id:"humbleEnthusiastic12", src:"sounds/humble_enthusiastic_12.mp3"},
-		{id:"humbleHappy02", src:"sounds/humble_happy_02.mp3"},
-		{id:"humbleScared01", src:"sounds/humble_scared_01.mp3"},
-		{id:"humbleScared02", src:"sounds/humble_scared_02.mp3"},
-		{id:"stumbleAffirmative07", src:"sounds/stumble_affirmative_07.mp3"},
-		{id:"stumbleEating01", src:"sounds/stumble_eating_01.mp3"},
-		{id:"stumbleEnthusiastic01", src:"sounds/stumble_enthusiastic_01.mp3"},
-		{id:"stumbleExcited06", src:"sounds/stumble_excited_06.mp3"},
-		{id:"stumbleHappyDistant", src:"sounds/stumble_happy_distant.mp3"},
-		{id:"stumbleHappy01", src:"sounds/stumble_happy_01.mp3"},
-		{id:"stumblePhew05", src:"sounds/stumble_phew_05.mp3"},
-		{id:"stumbleScared04", src:"sounds/stumble_scared_04.mp3"},
-		{id:"stumbleScared07", src:"sounds/stumble_scared_07.mp3"},
-		{id:"stumbleWow01", src:"sounds/stumble_wow_01.mp3"},
-		{id:"yetiHmm", src:"sounds/yeti_hmm.mp3"},
-		{id:"yetiArgh", src:"sounds/yeti_argh.mp3"},
-		{id:"yetiGrr", src:"sounds/yeti_grr.mp3"},
-		{id:"coverNarrator", src:"sounds/cover_audio.mp3"},
-		{id:"page1Line1", src:"sounds/page1_audio_line1.mp3"},
-		{id:"page1Line2", src:"sounds/page1_audio_line2.mp3"},
-		{id:"page1Line3", src:"sounds/page1_audio_line3.mp3"},
-		{id:"page1Line4", src:"sounds/page1_audio_line4.mp3"},
-		{id:"page2Line1", src:"sounds/page2_audio_line1.mp3"},
-		{id:"page3Line1", src:"sounds/page3_audio_line1.mp3"},
-		{id:"page3Line2", src:"sounds/page3_audio_line2.mp3"},
-		{id:"page4Line1", src:"sounds/page4_audio_line1.mp3"},
-		{id:"page4Line2", src:"sounds/page4_audio_line2.mp3"},
-		{id:"page5Line1", src:"sounds/page5_audio_line1.mp3"},
-		{id:"page5Line2", src:"sounds/page5_audio_line2.mp3"},
-		{id:"page5YetiLine1", src:"sounds/page5_audio_yeti_line1.mp3"},
-		{id:"page6Line1", src:"sounds/page6_audio_line1.mp3"},
-		{id:"page6YetiLine1", src:"sounds/page6_audio_yeti_line1.mp3"},
-		{id:"page7Line1", src:"sounds/page7_audio_line1.mp3"},
-		{id:"page7Line2", src:"sounds/page7_audio_line2.mp3"},
-		{id:"page8Line1", src:"sounds/page8_audio_line1.mp3"},
-		{id:"page8YetiLine1", src:"sounds/page8_audio_yeti_line1.mp3"},
-		{id:"page8YetiLine2", src:"sounds/page8_audio_yeti_line2.mp3"},
-		{id:"page9Line1", src:"sounds/page9_audio_line1.mp3"},
-		{id:"page10Line1", src:"sounds/page10_audio_line1.mp3"},
-		{id:"page10Line2", src:"sounds/page10_audio_line2.mp3"},
-		{id:"page10YetiLine1", src:"sounds/page10_audio_yeti_line1.mp3"},
-		{id:"page11Line1", src:"sounds/page11_audio_line1.mp3"},
-		{id:"page11YetiLine1", src:"sounds/page11_audio_yeti_line1.mp3"},
-		{id:"page12Humble", src:"sounds/page12_audio_humble.mp3"},
-		{id:"page12Line1", src:"sounds/page12_audio_line1.mp3"},
-		{id:"page12Line2", src:"sounds/page12_audio_line2.mp3"},
-		{id:"page12YetiLine1", src:"sounds/page12_audio_yeti_line1.mp3"},
-		{id:"page13Line1", src:"sounds/page13_audio_line1.mp3"},
-		{id:"page14Line1", src:"sounds/page14_audio_line1.mp3"},
-		{id:"page14YetiLine1", src:"sounds/page14_audio_yeti_line1.mp3"},
-		{id:"page15Grumble", src:"sounds/page15_audio_grumble.mp3"},
-		{id:"page15Line1", src:"sounds/page15_audio_line1.mp3"},
-		{id:"page15Line2", src:"sounds/page15_audio_line2.mp3"},
-		{id:"page15YetiLine1", src:"sounds/page15_audio_yeti_line1.mp3"},
-		{id:"page16Line1", src:"sounds/page16_audio_line1.mp3"},
-		{id:"page17Line1", src:"sounds/page17_audio_line1.mp3"},
-		{id:"page17Line2", src:"sounds/page17_audio_line2.mp3"},
-		{id:"page17YetiLine1", src:"sounds/page17_audio_yeti_line1.mp3"},
-		{id:"page18Line1", src:"sounds/page18_audio_line1.mp3"},
-		{id:"page18YetiLine1", src:"sounds/page18_audio_yeti_line1.mp3"},
-		{id:"theEnd", src:"sounds/page18_audio_theend.mp3"}
-	]
-}
+var index = 0;
+		
+var dragRightTolerance = 500;
+var dragLeftTolerance = -500;
+var transitionDelay = 350;
+var transitionEase = createjs.Ease.quintOut;
 
 function getStarted(){
 	lib = AdobeAn.getComposition(AdobeAn.bootcompsLoaded[0]).getLibrary();
-	preload = new lib.preloader();
-	clicker = new lib.click();
+	page0 = new lib.page0();
+	page1 = new lib.page1();
+	page2 = new lib.page2();
+	page3 = new lib.page3();
 
-	stage.addChild(preload);
+	PageArray = [page0, page1, page2, page3];
+	total = PageArray.length-1;
+	page = PageArray[index];
+	stage.addChild(page);
 
-	//* Pre load all the audio *//
-	var queue = new createjs.LoadQueue();
-	queue.installPlugin(createjs.Sound);
-	queue.on("complete", handleComplete);
+	createjs.Touch.enable(true);
+	stage.mouseMoveOutside = true;
+	page.on("mousedown", mouseDownHandler);
 
-	queue.loadManifest(sounds.library);
+	}
 
-	function handleComplete(){
-		if (!soundsLoaded) sounds.preloadFiles();
-		soundsLoaded = true;
+	function mouseDownHandler(e)
+	{
+		e.currentTarget.pressedX = e.currentTarget.x;
+		e.currentTarget.pressed = true;
+		e.currentTarget.offsetX = (e.stageX / stage.scaleX) - e.currentTarget.x;
+		e.currentTarget.on("pressmove", pressMoveHandler);
+		stage.addEventListener("stagemouseup", stageMouseUpHandler);
+	};
 
-		// load page according to cookie number
-		var pageIndex = getCookie("page");
-		var PageArray = [page0, page1, page2, page3, page4, page5, page6, page7, page8, page9, page10, page11, page12, page13, page14, page15, page16, page17, page18];
-		stage.removeChild(preload);
-		if(pageIndex >=1){
-			PageArray[pageIndex]();
-		} else {
-			page0();
+	function pressMoveHandler(e)
+	{
+		e.currentTarget.dragDistance = e.currentTarget.x - e.currentTarget.pressedX;
+		e.currentTarget.x = (e.stageX / stage.scaleX) - e.currentTarget.offsetX;		
+	};
+
+	function stageMouseUpHandler (e)
+	{
+		if (index < total && (page.dragDistance <= dragLeftTolerance))
+		{
+			createjs.Tween.get(page).to({x:-(canvas.width / stage.scaleX)}, transitionDelay, transitionEase).call(nextPage);
 		}
-	}
 
-	stage.on("stagemousedown", function(event){
-		stage.addChild(clicker);
-		var p = this.globalToLocal(event.stageX, event.stageY);
-		clicker.x = p.x;
-		clicker.y = p.y;
-		createjs.Ticker.addEventListener("tick", removeClicker);
-	});
+		if (index > 0 && (page.dragDistance >= dragRightTolerance))
+		{
+			createjs.Tween.get(page).to({x:(canvas.width / stage.scaleX)}, transitionDelay, transitionEase).call(previousPage);
+		} 
 
-	function removeClicker(){
-		if(clicker.currentFrame == 5){
-			stage.removeChild(clicker);
-			createjs.Ticker.removeEventListener("tick", removeClicker);
+		else {
+			createjs.Tween.get(page).to({x:0}, transitionDelay, transitionEase);
 		}
+
+		e.currentTarget.off("pressmove", pressMoveHandler);
+		stage.off("stagemouseup", stageMouseUpHandler);
+		e.currentTarget.pressed = false;
 	}
-}
 
-//* Cookie Logic *//
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
+	function nextPage(){
+		if (index <= total){
+			index++
+		}
+		stage.removeChild(page);
+		getStarted();
 	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
-}
 
-function getCookie(name) {
-  var cname = name + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(cname) == 0) {
-      return c.substring(cname.length, c.length);
-    }
-  }
-  return "";
-}
-
-function eraseCookie(name) {
-	createCookie(name,"",-1);
-}
+	function previousPage(){
+		if (index >= 0){
+			index--
+		}
+		console.log("index " + index);
+		getStarted();
+	}
 
 class Animations{
 
@@ -238,85 +144,6 @@ class Fade{
 			if (this.faded) {
 				this.faded = false;
 			}
-		}
-	}
-}
-
-function addNextButton(){
-
-	lib = AdobeAn.getComposition(AdobeAn.bootcompsLoaded[0]).getLibrary();
-	nextButton = new lib.nextbutton_mc();
-
-	stage.addChild(nextButton);
-	//height = stage.canvas.height / 2 + (window.screen.width * devicePixelRatio) / 2;
-	//width = stage.canvas.width / 2 + (window.screen.height * devicePixelRatio) / 2;
-	height = stage.canvas.height;
-	width = stage.canvas.width;
-	nextButton.y = height;
-	nextButton.x = width;
-	nextButton.alpha = 0;
-	createjs.Ticker.addEventListener("tick", fadeUpButton)
-	let nextUp = new Fade(nextButton);
-	
-	function fadeUpButton() {
-		nextUp.FadeUp();
-		if(nextUp.faded){
-			createjs.Ticker.removeEventListener("tick", fadeUpButton);
-		}
-	}
-}
-
-
-function removeNextButton(){
-
-	lib = AdobeAn.getComposition(AdobeAn.bootcompsLoaded[0]).getLibrary();
-	createjs.Ticker.addEventListener("tick", fadeDownButton)
-	let nextDown = new Fade(nextButton);
-	nextButtonAdded = false;
-	
-	function fadeDownButton() {
-		nextDown.FadeDown();
-		if(!nextDown.faded){
-			createjs.Ticker.removeEventListener("tick", fadeDownButton);
-		}
-	}
-}
-
-
-function addPreviousButton(){
-
-	lib = AdobeAn.getComposition(AdobeAn.bootcompsLoaded[0]).getLibrary();
-	previousButton = new lib.previousbutton_mc();
-
-	stage.addChild(previousButton);
-	//height = stage.canvas.height / 2 + (window.screen.width * devicePixelRatio) / 2;
-	//width = stage.canvas.width / 2 - (window.screen.height * devicePixelRatio) / 2;
-	height = stage.canvas.height;
-	previousButton.y = height;
-	//previousButton.x = width
-	previousButton.alpha = 0;
-	createjs.Ticker.addEventListener("tick", fadeUpButton);
-	let previousUp = new Fade(previousButton);
-	
-	function fadeUpButton() {
-		previousUp.FadeUp();
-		if(previousUp.faded){
-			createjs.Ticker.removeEventListener("tick", fadeUpButton);
-		}
-	}
-}
-
-function removePreviousButton(){
-
-	lib = AdobeAn.getComposition(AdobeAn.bootcompsLoaded[0]).getLibrary();
-	createjs.Ticker.addEventListener("tick", fadeDownButton)
-	let previousDown = new Fade(previousButton);
-	previousButtonAdded = false;
-	
-	function fadeDownButton() {
-		previousDown.FadeDown();
-		if(!previousDown.faded){
-			createjs.Ticker.removeEventListener("tick", fadeDownButton);
 		}
 	}
 }
